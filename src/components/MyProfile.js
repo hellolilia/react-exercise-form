f7db9f35e3c39b53daa21b6a24cdb7d1eee7438a
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-09-24 16:11:52 +0800

    completed function

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,12 +1,72 @@
 import React, {Component} from 'react';
 import './myProfile.less';
 
+const options =[{ value: 'male', label: 'male'},
+    { value: 'female', label: 'female'}];
+
 class MyProfile extends Component {
-  render() {
-    return (
-      <form></form>
-    );
-  }
+    state = {
+        name: '',
+        gender: 'male',
+        description: '',
+        tick: true
+    }
+
+    handleChange = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+    }
+
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(JSON.stringify(this.state))
+    }
+
+
+
+    render() {
+        return <form onSubmit={this.handleSubmit}>
+                <header>My Profile</header>
+                <label className='name'>
+                    Name
+                    <input type='text'
+                           name='name'
+                           onChange={this.handleChange}
+                           value={this.state.name}/>
+                </label>
+                <label  className='gender'>
+                    Gender
+                    <select value={this.state.value}
+                            name='gender'
+                            onChange={this.handleChange}>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                    </select>
+                </label>
+                <label className='description'>
+                    Description
+                    <input type='text'
+                           name='description'
+                           onChange={this.handleChange}
+                           value={this.state.description}/>
+                </label>
+                <input className='tick'
+                       type='checkbox'
+                       name='tick'
+                       checked={this.state.tick}
+                       onChange={this.handleChange}/>
+                I have read the terms of conduct
+                <input className='submit'
+                       type='submit'
+                       name='submit'
+                       disabled={!this.state.name || !this.state.description || !this.state.tick}/>
+            </form>
+    }
 }
 
 export default MyProfile;
